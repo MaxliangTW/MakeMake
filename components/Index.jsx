e564277f0d8fc974be8964e0ce3c0c1f67f740ff
max <88110max123@gmail.com>
2024-04-29 23:08:22 +0800

    add(web)first push to github

---
+++ components/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Narbar from './Navbar'
+
+function Index() {
+    return (
+        <div>
+            <Narbar />
+        </div>
+    );
+}
+
+export default Index;
